456c9fdce0c0794d35ab42483dfc09c9cdb6b47e
earifoglu <143426905+earifoglu@users.noreply.github.com>
2024-09-12 20:59:30 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -14,52 +14,58 @@ public class Q1WeirdFunction {
     
     public static int fIterative(int n) {
         // TODO: Implement the iterative function
-        
-        if(n < 3) {
-            return n;
+        int i = 0;
+        ArrayList<int> funcNums = new ArrayList<int>();
+        int res = 0;
+
+        if(n >= 3) {
+            if(funcNums.size() == i) {
+                funcNums.add(1);
+            }
+            if(funcNums.get(i) == 1) {
+                n--;
+            }
+            else if(funcNums.get(i) == 2) {
+                n -= 2;
+            }
+            else if(funcNums.get(i) == 3) {
+                n -= 3;
+            }
+            if(n >= 3) {
+                i++;
+            }
+
         }
         else {
-            ArrayList funcNums = new ArrayList<int>();
-            int i = 0;
-            int res = 0;
-            
-            while(true) {
-                if(n >= 3) {
-                    funcNums.add(1);
-                    if(funcNums.get(i) == 1){
-                        n--;
+            if(funcNums[i] == 1) {
+                res += n;
+                n++;
+                funcNums.set(i, funcNums.get(i) + 1);
+            }
+            else if(funcNums[i] == 2) {
+                res +=  2 * n;
+                n += 2;
+                funcNums.set(i, funcNums.get(i) + 1);
+            }
+            else if(funcNums[i] == 3) {
+                res += 3 * n;
+                n += 3;
+                funcNums.remove(i);
+                i--;
+                while(true) {
+                    if(funcNums[i] == 3) {
+                        n += 3;
+                        funcNums.remove(i);
                     }
-                    else if(funcNums.get(i) == 2) {
-                        n -= 2;
-                    }
-                    else if(funcNums.get(i) == 3) {
-                        n -= 3
-                    }
-                    i++;
-                }
-                else {
-                    
-                    if(funcNums.get(i - 1) == 1){
-                        res += n;
-                        n++;
-                    }
-                    else if(funcNums.get(i - 1) == 2) {
-                        res += 2 * n
-                        n += 2;
-                    }
-                    else if(funcNums.get(i - 1) == 3) {
-                        res += 3 * n
-                        n += 3
-                    }
-
-                    funcNums.set(i - 1, funcNums.get(i - 1) + 1);
-                    if(funcNums.get(i - 1) == 3) {
-                        funcNums.remove(i - 1);
-                    
-                        i--;
-                        funcNums.set(i, funcNums.get(i)++);
+                    else {
+                        if(funcNums[i] == 1) {
+                            n += 1;
+                            funcNums.set(i, funcNums.get(i) + 1);
+                            break;
+                        }
                     }
                 }
             }
+        }
     }
 }
