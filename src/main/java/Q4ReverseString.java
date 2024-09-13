b50ff790d039719121da5f030ca29c2211bf507a
earifoglu <143426905+earifoglu@users.noreply.github.com>
2024-09-13 00:33:45 -0400

    Update Q4ReverseString.java

--- src/main/java/Q4ReverseString.java
+++ src/main/java/Q4ReverseString.java
@@ -4,7 +4,7 @@ public class Q4ReverseString {
 
     public static String reverse(String s) {
         // TODO: Implement the recursive function to reverse the string
-        String str;
+        String str = "";
         if(s.length() > 1) {
             str = s.charAt(s.length() - 1)  + reverse(s.substring(0, s.length() - 1));
         }
